90107bc24b69718d4b9a9be6f9dcb980df34c799
Sami-Saqr <140464418+Sami-Saqr@users.noreply.github.com>
2025-07-13 01:37:22 +0300

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter two integers: ";
-    cin >> a >> b;
+    cin>>a>>b;
 
     if (a == b)
-        cout << "Numbers are equal" << endl;
+        cout << "equal" << endl;
     else
-        cout << "Numbers are not equal" << endl;
+        cout << "not equal" << endl;
 
     return 0;
 }
